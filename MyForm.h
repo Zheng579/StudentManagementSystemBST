e33d358c52b5fd65b44771acc5572a2df72e7f88
Kang Zheng <leekangzheng97@gmail.com>
2024-11-24 13:23:45 +0800

    feat: add create and update update/delete function

--- MyForm.h
+++ MyForm.h
@@ -52,6 +52,11 @@ namespace InventoryManagementBST {
         System::Windows::Forms::DataGridView^ inventoryGridView;
 
         System::ComponentModel::Container^ components;
+        System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn1;
+        System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn2;
+        System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn3;
+        System::Windows::Forms::DataGridViewButtonColumn^ Action;
+        System::Windows::Forms::DataGridViewButtonColumn^ Delete;
 
         // Local inventory data
         System::Collections::Generic::List<InventoryItem^>^ inventoryList = gcnew System::Collections::Generic::List<InventoryItem^>();
@@ -63,54 +68,71 @@ namespace InventoryManagementBST {
             this->searchAllButton = (gcnew System::Windows::Forms::Button());
             this->inputTextBox = (gcnew System::Windows::Forms::TextBox());
             this->inventoryGridView = (gcnew System::Windows::Forms::DataGridView());
+            this->dataGridViewTextBoxColumn1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+            this->dataGridViewTextBoxColumn2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+            this->dataGridViewTextBoxColumn3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+            this->Action = (gcnew System::Windows::Forms::DataGridViewButtonColumn());
+            this->Delete = (gcnew System::Windows::Forms::DataGridViewButtonColumn());
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->inventoryGridView))->BeginInit();
             this->SuspendLayout();
-
             // 
             // searchAllButton
             // 
             this->searchAllButton->Location = System::Drawing::Point(20, 20);
             this->searchAllButton->Name = L"searchAllButton";
             this->searchAllButton->Size = System::Drawing::Size(100, 30);
+            this->searchAllButton->TabIndex = 0;
             this->searchAllButton->Text = L"Search All";
             this->searchAllButton->UseVisualStyleBackColor = true;
             this->searchAllButton->Click += gcnew System::EventHandler(this, &MyForm::SearchAllButton_Click);
-
             // 
             // inputTextBox
             // 
             this->inputTextBox->Location = System::Drawing::Point(140, 25);
             this->inputTextBox->Name = L"inputTextBox";
             this->inputTextBox->Size = System::Drawing::Size(300, 20);
-
+            this->inputTextBox->TabIndex = 1;
             // 
             // inventoryGridView
             // 
+            this->inventoryGridView->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+            this->inventoryGridView->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(5) {
+                this->dataGridViewTextBoxColumn1,
+                    this->dataGridViewTextBoxColumn2, this->dataGridViewTextBoxColumn3, this->Action, this->Delete
+            });
             this->inventoryGridView->Location = System::Drawing::Point(20, 70);
             this->inventoryGridView->Name = L"inventoryGridView";
             this->inventoryGridView->Size = System::Drawing::Size(800, 500);
-            this->inventoryGridView->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-
-            // Adding columns
-            this->inventoryGridView->Columns->Add("InventoryId", "Inventory Id");
-            this->inventoryGridView->Columns->Add("InventoryDescription", "Inventory Description");
-            this->inventoryGridView->Columns->Add("Quantity", "Quantity");
-
-            // Add Delete and Update button columns
-            DataGridViewButtonColumn^ deleteButton = gcnew DataGridViewButtonColumn();
-            deleteButton->HeaderText = "Action";
-            deleteButton->Text = "Delete";
-            deleteButton->UseColumnTextForButtonValue = true;
-            this->inventoryGridView->Columns->Add(deleteButton);
-
-            DataGridViewButtonColumn^ updateButton = gcnew DataGridViewButtonColumn();
-            updateButton->HeaderText = "";
-            updateButton->Text = "Update";
-            updateButton->UseColumnTextForButtonValue = true;
-            this->inventoryGridView->Columns->Add(updateButton);
-
-            // Add event handler for button clicks
+            this->inventoryGridView->TabIndex = 2;
             this->inventoryGridView->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::InventoryGridView_CellContentClick);
-
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this->dataGridViewTextBoxColumn1->HeaderText = L"Inventory Id";
+            this->dataGridViewTextBoxColumn1->Name = L"dataGridViewTextBoxColumn1";
+            this->dataGridViewTextBoxColumn1->ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this->dataGridViewTextBoxColumn2->HeaderText = L"Inventory Description";
+            this->dataGridViewTextBoxColumn2->Name = L"dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this->dataGridViewTextBoxColumn3->HeaderText = L"Quantity";
+            this->dataGridViewTextBoxColumn3->Name = L"dataGridViewTextBoxColumn3";
+            // 
+            // Action
+            // 
+            this->Action->HeaderText = L"Update";
+            this->Action->Name = L"Action";
+            this->Action->Text = L"Update";
+            // 
+            // Delete
+            // 
+            this->Delete->HeaderText = L"Delete";
+            this->Delete->Name = L"Delete";
+            this->Delete->Text = L"Delete";
             // 
             // MyForm
             // 
@@ -122,8 +144,10 @@ namespace InventoryManagementBST {
             this->Controls->Add(this->inventoryGridView);
             this->Name = L"MyForm";
             this->Text = L"Inventory Management";
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->inventoryGridView))->EndInit();
             this->ResumeLayout(false);
             this->PerformLayout();
+
         }
 #pragma endregion
 
@@ -149,7 +173,7 @@ namespace InventoryManagementBST {
 
                 inventoryList->Add(gcnew InventoryItem(id, description, quantity));
             }
-
+            PopulateGridView();
             file.close();
         }
 
@@ -191,33 +215,34 @@ namespace InventoryManagementBST {
                     return; // Return if input is not a valid number
                 }
             }
-            else
-            {
-                // If input is empty, show all items
-                LoadDataFromJson(); // Re-load the data from the JSON file
-            }
-
-            PopulateGridView(); // Populate the grid with the filtered or full list
         }
 
         // Handle update and delete actions
         void InventoryGridView_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e)
         {
-            if (e->RowIndex >= 0)
+            if (e->RowIndex >= 0) // Ensure valid row index
             {
-                if (e->ColumnIndex == 3) // Delete button
+                switch (e->ColumnIndex)
                 {
-                    int inventoryId = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[0]->Value);
-                    DeleteInventoryData(inventoryId);
-                }
-                else if (e->ColumnIndex == 4) // Update button
-                {
-                    int inventoryId = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[0]->Value);
-                    String^ description = this->inventoryGridView->Rows[e->RowIndex]->Cells[1]->Value->ToString();
-                    int quantity = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[2]->Value);
+                    case 3: // Update button column
+                    {
+                        int inventoryId = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[0]->Value);
+                        String^ description = this->inventoryGridView->Rows[e->RowIndex]->Cells[1]->Value->ToString();
+                        int quantity = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[2]->Value);
 
-                    // Update the inventory directly within the list
-                    UpdateInventoryData(inventoryId, description, quantity);
+                        // Update the inventory directly within the list
+                        UpdateInventoryData(inventoryId, description, quantity);
+                        break;
+                    }
+                    case 4: // Delete button column
+                    {
+                        int inventoryId = Convert::ToInt32(this->inventoryGridView->Rows[e->RowIndex]->Cells[0]->Value);
+                        DeleteInventoryData(inventoryId);
+                        break;
+                    }
+                    default:
+                        // Optional: Handle other cases or do nothing
+                        break;
                 }
             }
         }
@@ -225,13 +250,21 @@ namespace InventoryManagementBST {
         // Update inventory data directly in the list
         void UpdateInventoryData(int inventoryId, String^ description, int quantity)
         {
-            for each (InventoryItem ^ item in inventoryList)
-            {
-                if (item->InventoryId == inventoryId)
+            if (inventoryId == 0) {
+                InventoryItem^ lastItem = inventoryList[inventoryList->Count - 1];
+                int newId = lastItem->InventoryId + 1;  // Assuming 'ID' is a public property of 'item'
+
+                inventoryList->Add(gcnew InventoryItem(newId, description, quantity));
+            }
+            else {
+                for each (InventoryItem ^ item in inventoryList)
                 {
-                    item->Description = description;
-                    item->Quantity = quantity;
-                    break;
+                    if (item->InventoryId == inventoryId)
+                    {
+                        item->Description = description;
+                        item->Quantity = quantity;
+                        break;
+                    }
                 }
             }
 
